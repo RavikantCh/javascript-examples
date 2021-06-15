e8cb7c65ae90c961e25caca5ab1210b8fe54e278
RavikantCh <ravikant.testing@gmail.com>
2021-06-15 08:47:24 +0530

    First commit

---
+++ basics/HelloWorld.js
@@ -0,0 +1,9 @@
+console.log('Hello World - Javascript !!!')
+
+let firstName = "Jonas";
+console.log(firstName);
+
+
+
+
+
---
+++ basics/datatypes.js
@@ -0,0 +1,5 @@
+console.log('Data types - Primitive types')
+
+let age = 25;
+let iceCreamCost = 10.25;
+let isMarried = false;
